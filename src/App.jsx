4446e779e37be4cfede1a590889d61cdeda9eb05
BeibeiYang <chrisyangber@hotmail.com>
2024-07-15 23:25:27 +1000

    setup tailwind and Daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export const App = () => {
+  return (
+    <div className="bg-purple-500">
+      <h1 className="text-xl">Hello World</h1>
+      <button className="btn">Click</button>
+    </div>
+  );
+};
