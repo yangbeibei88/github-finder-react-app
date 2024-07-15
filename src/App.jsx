9b815e82d50d749c36ca49f953eea709be9829fc
BeibeiYang <chrisyangber@hotmail.com>
2024-07-16 00:16:14 +1000

    navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Navbar } from "./components/layout/Navbar.jsx";
+
 export const App = () => {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello World</h1>
-      <button className="btn">Click</button>
-    </div>
+    <BrowserRouter>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar title={"Github Finder"} />
+
+        <main>Content</main>
+      </div>
+    </BrowserRouter>
   );
 };
