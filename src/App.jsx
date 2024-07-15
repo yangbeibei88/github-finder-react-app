8c0e7ae42c5fb8754699ecff109acc21151c8587
BeibeiYang <chrisyangber@hotmail.com>
2024-07-16 00:55:54 +1000

    footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Navbar } from "./components/layout/Navbar.jsx";
+import { Footer } from "./components/layout/Footer.jsx";
 
 export const App = () => {
   return (
@@ -7,7 +8,8 @@ export const App = () => {
       <div className="flex flex-col justify-between h-screen">
         <Navbar title={"Github Finder"} />
 
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Footer />
       </div>
     </BrowserRouter>
   );
